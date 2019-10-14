d607c5e91d1a89316e0e1daf2b5abbfe4c6246f7
Karol Baryła <lorak.mmk@gmail.com>
2019-10-15 01:38:45 +0200

    Added travel query parsing

--- kasa.cc
+++ kasa.cc
@@ -1,23 +1,24 @@
 #include <algorithm>
-#include <chrono>
 #include <iostream>
 #include <regex>
 #include <string>
 
-using time_point = pair<int, int> using duration = std::chrono::duration;
-using stops = std::unordered_map<string, time_point>;
+using time_point = pair<int, int>;
+using duration = std::chrono::duration;
+using stops = std::unordered_map<std::string, time_point>;
 // trzymamy cenę biletu pomnożoną przez 100
-using single_ticket = std::tuple<string, long long, long long>;
+// (nazwa, cena, długość)
+using single_ticket = std::tuple<std::string, long long, long long>;
 // każdy element to odpowiednio nazwa przystanku, numer kursu
-using travel = std::vector<string, long long>;
+using travel = std::vector<std::string, long long>;
 
-std::unordered_map<long long, stops> courses;
+static const std::regex add_course_regex(
+    "(\\d+)((?: (?:(?:\\d|1\\d|2[0-3]):[0-5]\\d) (?:[a-zA-Z_^]+))+)");
+static const std::regex add_ticket_regex("([a-zA-Z ]+) (\\d+)\\.(\\d{2}) ((?!0)\\d+)");
+static const std::regex travel_query_regex("\\? ([a-zA-Z_^]+(?: \\d+ [a-zA-Z_^]+)+)");
 
-static const std::regex add_course_regex("(\\d+)((?: (?:\\d{1,2}:[0-5]\\d) (?:[a-zA-Z_^]+))+)");
-static const std::regex add_ticket_regex("([a-zA-Z ]+) (\\d+\\.\\d{2}) ((?!0)\\d+)");
-static const std::regex travel_query_regex("\\? ([a-zA-Z_^]+(?: \\d+ [a-zA-Z_^]+)*)");
-
-std::vector<single_tickets> tickets;
+static const time_point opening_time = std::make_pair(5, 55);
+static const time_point closing_time = std::make_pair(21, 21);
 
 bool equal_time_points(time_point a, time_point b) {
   if (a.first == b.first && a.second == b.second) return true;
@@ -66,7 +67,7 @@ std::vector<string> buy_tickets(int travel_time) {
 }
 
 // current_travel zawiera opis niepustej podróży
-// funkcja time_of_connection zwraca nieujemną liczbę reprezentującą 
+// funkcja time_of_connection zwraca nieujemną liczbę reprezentującą
 // sumaryczny czas podróży
 // lub liczbę ujemną równą current_travel.size(), gdy dane wejściowe są niepoprawne
 // inna liczba ujemna reprezentuje indeks przystanku, na którym trzeba czekać
@@ -113,30 +114,61 @@ void print_error(size_t line_number, std::string line) {
   std::cerr << "Error in line " << line_number << ": " << line << "\n";
 }
 
-std::pair<int, stops> parse_course(std::smatch sm) {
-  int number = std::stoi(sm[1].str());  // TODO: handle exceptions to catch numbers that are too big
+std::pair<long long, stops> parse_course(std::smatch sm) {
+  long long number =
+      std::stoll(sm[1].str());  // TODO: handle exceptions to catch numbers that are too big
+  stops course;
+
   std::vector<std::string> v;
   string_split(sm[2].str(), v);
+  time_point previous = opening_time;
   for (int i = 0; i < v.size() - 1; i += 2) {
     std::vector<std::string> time;
     string_split(v[i], time, ':');
     time_point tp = std::make_pair(std::stoi(time[0]), std::stoi(time[1]));
+    if (tp <= previous) {
+      // TODO: handle error
+    }
+    previous = tp;
     std::string stop = v[i + 1];
+    if (course.count(stop)) {
+      // TODO: handle error
+    }
+    course[stop] = tp;
   }
+
+  return std::make_pair(number, course);
 }
 
 single_ticket parse_ticket(std::smatch sm) {
   std::string name = sm[1].str();
-  double price = std::stod(sm[2].str());
-  time_point tp = std::make_pair(0, 0);  // TODO: Let's discuss form of time_point first. Also, if
-                                         // time_point ends up being from std::chrono, then this
-                                         // should be duration from std::chrono
+  // Część całkowita i ułamkowa w osobnych grupach, by łatwo było je złączyć
+  long long price = std::stoll(sm[2].str() + sm[3].str());
+  long long duration =
+      std::stoll(sm[4].str());  // TODO: handle exceptions to catch numbers that are too big
   return std::make_tuple(name, price, tp);
 }
 
-travel parse_travel_query(std::smatch sm) {}
+travel parse_travel_query(std::smatch sm) {
+  std::string travel_str = sm[1].str();
+  std::vector<std::string> args;
+  string_split(travel_str, args);
+  travel result;
+
+  for (int i = 0; i < args.length - 2; i += 2) {
+    result.push_back(std::make_pair(args[i], args[i + 1]));
+  }
+  result.push_back(std::make_pair(args[args.length() - 1], -1));
+
+  return result;
+}
 
 int main(int argc, char* argv[]) {
+  // courses: numer linii -> rozkład
+  std::unordered_map<long long, stops> courses;
+  // tickets: lista biletów
+  std::vector<single_tickets> tickets;
+
   std::string line;
   size_t line_number = 0;
 
@@ -146,9 +178,19 @@ int main(int argc, char* argv[]) {
 
     std::smatch sm;
     if (std::regex_match(line, sm, add_course_regex)) {
-      std::pair<int, stops> course = parse_course(sm);
+      std::pair<long long, stops> course = parse_course(sm);
+          if(courses.count(course.first){
+        // TODO: handle error
+	  }
+	  courses[course.first] = course.second;
     } else if (std::regex_match(line, sm, add_ticket_regex)) {
       single_ticket ticket = parse_ticket(sm);
+      for (auto t : tickets) {
+        if (std::get<0>(t) == std::get<0>(ticket)) {
+          // TODO: handle error
+        }
+      }
+      tickets.push_back(ticket);
     } else if (std::regex_match(line, sm, travel_query_regex)) {
       travel query = parse_travel_query(sm);
     } else {
