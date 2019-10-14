7c2b30acc1d7f283611545dfdcfeb23c091606dc
esculapa <jadzia.cz21@gmail.com>
2019-10-14 13:12:19 +0200

    kasaponiedzialek

--- kasa.cc
+++ kasa.cc
@@ -1,52 +1,64 @@
 #include <algorithm>
-#include <cfloat>
+#include <chrono>
 #include <iostream>
 #include <regex>
-#include <stdlib>
 #include <string>
 
-using time_point = pair<int, int>;
-using stops = unordered_map<string, time_point>;
-using single_ticket = tuple<string, double, time_point>;
+using time_point = pair<int, int> using duration = std::chrono::duration;
+using stops = std::unordered_map<string, time_point>;
+// trzymamy cenę biletu pomnożoną przez 100
+using single_ticket = std::tuple<string, long long, long long>;
 // każdy element to odpowiednio nazwa przystanku, numer kursu
-using travel = vector<string, int>;
+using travel = std::vector<string, long long>;
+
+std::unordered_map<long long, stops> courses;
 
 static const std::regex add_course_regex("(\\d+)((?: (?:\\d{1,2}:[0-5]\\d) (?:[a-zA-Z_^]+))+)");
 static const std::regex add_ticket_regex("([a-zA-Z ]+) (\\d+\\.\\d{2}) ((?!0)\\d+)");
 static const std::regex travel_query_regex("\\? ([a-zA-Z_^]+(?: \\d+ [a-zA-Z_^]+)*)");
 
-unordered_map<int, stops> courses;
-vector<single_tickets> tickets;
+std::vector<single_tickets> tickets;
 
-int time_distance(time_point a, time_point b) {
-  return abs(a.first - b.first) * 60 + abs(a.second - b.second);
-}
-
-bool equal_times(time_point a, time_point b) {
+bool equal_time_points(time_point a, time_point b) {
   if (a.first == b.first && a.second == b.second) return true;
   return false;
 }
 
 // travel_time - czas, na który mamy kupić bilety podany w minutach
-vector<string> buy_tickets(int travel_time) {
+std::vector<string> buy_tickets(int travel_time) {
   long double smallest_cost = LDBL_MAX;
-  vector<string> best_tickets;
+  std::vector<string> best_tickets;
 
-  for (int i = 0; i < tickets; i++) {
-    time_point summed_time = get<2> tickets[i];
-    long double summed_cost = get<1> tickets[i];
+  for (size_t) i = 0;
+  i < tickets.size(); i++){
+    time_point summed_time = std::get<2>(tickets[i]);
+    long double summed_cost = std::get<1>(tickets[i]);
     if (ok_czas_biletu && summed_cost < smallest_cost) {
       smallest_cost = summed_cost;
       best_tickets.clear();
-      best_tickets.push_back(get<0> tickets[i]);
+      best_tickets.push_back(std::get<0>(tickets[i]));
       continue;
     }
-    for (int j = i; j < tickets; j++) {
-      for (int k = j; k < tickets; k++) {
+    for (size_t j = i; j < tickets.size(); j++) {
+      summed_cost = std::get<1>(tickets[i]) + std::get<1>(tickets[j]);
+      summed_time = std::get<2>(tickets[i]) + std::get<2>(tickets[j]);
+      if (ok_czas_biletow && summed_cost < smallest_cost) {
+        smallest_cost = summed_cost;
+        best_tickets.clear();
+        best_tickets.push_back(std::get<0>(tickets[i]));
+        best_tickets.push_back(std::get<0>(tickets[j]));
+        continue;
+      }
+      for (size_t k = j; k < tickets.size(); k++) {
+        summed_cost = std::get<1>(tickets[i]) + std::get<1>(tickets[j]) + std::get<1>(tikcets[k]);
+        summed_time = std::get<2>(tickets[i]) + std::get<2>(tickets[j]) + std::get<2>(tickets[k]);
         if (ok_czas biletow && summed_cost < smallest_cost) {
           best_tickets.clear();
-          best_tickets.push_back(get<0> tickets[0]);
-          best_tickers.push_back smallest_cost = summed_cost;
+          best_tickets.push_back(std::get<0>(tickets[i]));
+          best_tickets.push_back(std::get<0>(tickets[j]));
+          best_tickets.push_back(std::get<0>(tickets[k]));
+          smallest_cost = summed_cost;
+          break;
         }
       }
     }
@@ -56,13 +68,12 @@ vector<string> buy_tickets(int travel_time) {
 // current_travel zawiera opis niepustej podróży
 // funkcja time_of_connection zwraca nieujemną liczbę reprezentującą 
 // sumaryczny czas podróży
-// lub liczbę ujemną równą current_travel.size(), gdy dane wejściowe są
-// niepoprawne inna liczba ujemna reprezentuje indeks przystanku, na którym
-// trzeba czekać
+// lub liczbę ujemną równą current_travel.size(), gdy dane wejściowe są niepoprawne
+// inna liczba ujemna reprezentuje indeks przystanku, na którym trzeba czekać
 int time_of_connection(travel current_travel) {
   time_point current_time, start_point;
 
-  for (int i = 0; i < current_travel.size() - 1; i++) {
+  for (size_t i = 0; i < current_travel.size() - 1; i++) {
     int course_number = current_travel[i].second;
     stops course = find(courses, course_number);
 
@@ -86,7 +97,7 @@ int time_of_connection(travel current_travel) {
       current_time = departure_from_stop;
       start_point = departure_from_stop;
     } else {
-      if (!equal_times(current_time, departure_from_stop)) {
+      if (current_time == departure_from_stop) {
         // trzeba czekać na tym przystanku
         return -i;
       } else {
@@ -98,28 +109,20 @@ int time_of_connection(travel current_travel) {
   return current_time - start_point;
 }
 
-void string_split(const std::string& str, vector<std::string>& v, const char d = ' '){
-    std::stringstream ss(str);
-    std::string token;
-    while(std::getline(ss, token, d)){
-        if(token.length() > 0) v.push_back(token);
-    }
-}
-
 void print_error(size_t line_number, std::string line) {
   std::cerr << "Error in line " << line_number << ": " << line << "\n";
 }
 
 std::pair<int, stops> parse_course(std::smatch sm) {
-    int number = std::stoi(sm[1].str()); //TODO: handle exceptions to catch numbers that are too big
-    std::vector<std::string> v;
-    string_split(sm[2].str(), v);
-    for(int i = 0; i < v.size() - 1; i += 2){
-        std::vector<std::string> time;
-        string_split(v[i], time, ':');
-        time_point tp = std::make_pair(std::stoi(time[0]), std::stoi(time[1]));
-        std::string stop = v[i + 1];
-    }
+  int number = std::stoi(sm[1].str());  // TODO: handle exceptions to catch numbers that are too big
+  std::vector<std::string> v;
+  string_split(sm[2].str(), v);
+  for (int i = 0; i < v.size() - 1; i += 2) {
+    std::vector<std::string> time;
+    string_split(v[i], time, ':');
+    time_point tp = std::make_pair(std::stoi(time[0]), std::stoi(time[1]));
+    std::string stop = v[i + 1];
+  }
 }
 
 single_ticket parse_ticket(std::smatch sm) {
@@ -131,9 +134,7 @@ single_ticket parse_ticket(std::smatch sm) {
   return std::make_tuple(name, price, tp);
 }
 
-travel parse_travel_query(std::smatch sm) {
-    
-}
+travel parse_travel_query(std::smatch sm) {}
 
 int main(int argc, char* argv[]) {
   std::string line;
