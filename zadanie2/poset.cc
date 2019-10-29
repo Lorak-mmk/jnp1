f4e75bfc8e3994410b6a761f0227d73550fd5088
Wookash <wookashinio@gmail.com>
2019-10-29 16:24:31 +0100

    jeszcze nie kompilowane

--- zadanie2/poset.cc
+++ zadanie2/poset.cc
@@ -116,4 +116,102 @@ bool poset_add(unsigned long id, char const *value1, char const *value2){
     std::string val1 = std::string(value1);
     std::string val2 = std::string(value2);
     
+    if(strcmp(val1.c_str(), val2.c_str()) != 0){
+        auto biggerId = biggerThan.find(id);
+        if(biggerId != biggerThan.end()){
+
+            auto checkVal1 = (*biggerId).second.find(val1);
+            auto checkVal2 = (*biggerId).second.find(val2);
+
+            if(checkVal1 != (*biggerId).second.end() && checkVal2 != (*biggerId).second.end()){
+
+                auto smallerId = smallerThan.find(id);
+
+                // Dodaj value1 jako poprzednik value2, oraz jako poprzednik dla wszystkich nastepnikow value2
+                auto smallerMap = (*smallerId).second.find(val2);
+                for(auto i = (*smallerMap).second.begin(); i != (*smallerMap).second.end(); i++){
+                    auto current = (*smallerId).second.find((*i));
+                    (*current).second.insert(val1);
+                }
+                (*smallerMap).second.insert(val1);
+
+                // Dodaj value2 jako nastepnik value1, oraz jako nastepnik dla wszystkich poprzednikow value1
+                for(auto i = (*checkVal1).second.begin(); i != (*checkVal1).second.end(); i++){
+                    auto current = (*biggerId).second.find(*i);
+                    (*current).second.insert(val2);
+                }
+                (*checkVal2).second.insert(val2);
+
+                return true;
+
+            }else return false;
+
+        }else return false;
+    }else return false;
+}
+
+bool poset_del(unsigned long id, char const *value1, char const *value2){
+
+    std::string val1 = std::string(value1);
+    std::string val2 = std::string(value2); 
+
+    if(strcmp(val1.c_str(), val2.c_str()) != 0){
+        auto biggerId = biggerThan.find(id);
+        if(biggerId != biggerThan.end()){
+
+            auto checkVal1 = (*biggerId).second.find(val1);
+            auto checkVal2 = (*biggerId).second.find(val2);
+
+            if(checkVal1 != (*biggerId).second.end() && checkVal2 != (*biggerId).second.end()){
+
+                auto smallerId = smallerThan.find(id);
+
+                // Usun value1 jako poprzednik value2, oraz jako poprzednik dla wszystkich nastepnikow value2
+                auto smallerMap = (*smallerId).second.find(val2);
+                for(auto i = (*smallerMap).second.begin(); i != (*smallerMap).second.end(); i++){
+                    auto current = (*smallerId).second.find((*i));
+                    (*current).second.erase(val1);
+                }
+                (*smallerMap).second.erase(val1);
+
+                // Usun value2 jako nastepnik value1, oraz jako nastepnik dla wszystkich poprzednikow value1
+                for(auto i = (*checkVal1).second.begin(); i != (*checkVal1).second.end(); i++){
+                    auto current = (*biggerId).second.find(*i);
+                    (*current).second.erase(val2);
+                }
+                (*checkVal2).second.erase(val2);
+
+                return true;
+
+            }else return false;
+
+        }else return false;
+    }else return false;
+}
+
+bool poset_test(unsigned long id, char const *value1, char const *value2){
+
+    std::string val1 = std::string(value1);
+    std::string val2 = std::string(value2);
+
+    auto checkId = biggerThan.find(id);
+
+    if(checkId != biggerThan.end()){
+
+        auto checkVal1 = (*checkId).second.find(val1);
+        auto checkVal2 = (*checkId).second.find(val2);
+        
+        if(checkVal1 != (*checkId).second.end() && checkVal2 != (*checkId).second.end()){
+
+            if(strcmp(val1.c_str(), val2.c_str()) == 0) return true;
+            else    return (*checkVal1).second.find(val2) != (*checkVal1).second.end();
+
+        }
+
+    }else return false;
+}
+
+void poset_clear(unsigned long id){
+    biggerThan.erase(id);
+    smallerThan.erase(id);
 }
