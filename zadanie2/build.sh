20b2ba8a8d2001a061503842158cc5ae7dd7cfe8
Karol Baryła <lorak.mmk+kde@gmail.com>
2019-10-29 21:49:28 +0100

    Added build script

---
+++ zadanie2/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o xtrace
+
+mkdir -p build
+
+g++ -Wall -Wextra -O2 -std=c++17 -c poset.cc -o build/poset.o
+
+gcc -Wall -Wextra -O2 -std=c11 -c poset_example1.c -o build/poset_example1.o
+g++ poset_example1.o poset.o -o poset_example1
+
+g++ -Wall -Wextra -O2 -std=c++17 -c poset_example2.cc -o build/poset_example2.o
+g++ poset_example2.o poset.o -o poset_example2
+
