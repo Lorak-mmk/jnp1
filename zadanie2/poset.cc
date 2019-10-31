f700af6398923c090d65bd09099bcd65854fbb73
k-baryla <k.baryla@samsung.com>
2019-10-31 20:16:24 +0100

    Refactoring

--- zadanie2/poset.cc
+++ zadanie2/poset.cc
@@ -34,7 +34,7 @@ namespace {
   // Wrapper for creating functions allowing to fix static order initialization.
   // Won't work if 2 global vars have same type, so don't use it like that.
   // You can work around it, by adding second template argument unsigned int N
-  // and passing to it from example __COUNTER__ macro or some other unique identifier
+  // and passing to it for example __COUNTER__ macro or some other unique identifier.
   template<typename T>
   T& global_var() {
     static T* val = new T();
@@ -46,11 +46,15 @@ namespace {
   auto poset_counter = global_var<id_counter_t<unsigned long>>;
   auto element_counter = global_var<id_counter_t<element_id_t>>;
   
+  // Code may be executed before main(), so we need to make sure that streams are available.
   inline void assert_stream_init() {
     static std::ios_base::Init* INIT = new std::ios_base::Init();
     (void)INIT;
   }
 
+  // Get new available id from counter.
+  // If there are freed ids available, will return one of them.
+  // If not, returns new id and increments internal counter.
   template <typename T>
   T get_new_id(id_counter_t<T> &counter) {
     std::queue<T> &que = std::get<0>(counter);
@@ -64,20 +68,36 @@ namespace {
     }
   }
 
+  // Free id taken from counter.
   template <typename T>
   void free_id(id_counter_t<T> &counter, T id) {
     std::get<0>(counter).emplace(id);
   }
 
+  // string_view doesn't destroy underlying char array upon destruction
+  // so we need to do it manually before clearing map with string_view keys.
   void delete_underlying_string(const std::string_view &s) { free((void *)s.data()); }
 
+  poset_t* poset_try_get(unsigned long id) {
+    auto it = posets().find(id);
+    if(it == posets().end()){
+      return nullptr;
+    }
+    
+    return it->second;
+  }
+  
   void poset_clear_elements(poset_t *poset) {
     for (auto &it : *poset) {
       delete_underlying_string(it.first);
+      elements().erase(it.second);
     }
     poset->clear();
   }
 
+  // Check if id1 preceeds id2.
+  // This is true, if id1 == id2 (because poset is reflexive),
+  // or id2 is in id1's successor list.
   bool poset_test_relation(element_id_t id1, element_id_t id2) {
     if(id1 == id2) return true;
     
@@ -85,6 +105,7 @@ namespace {
     return std::get<1>(*base).find(id2) != std::get<1>(*base).end();
   }
   
+  // Create new set, which is copy of main, but without elements that are also in to_remove.
   template<typename T>
   std::unordered_set<T> get_set_diff(const std::unordered_set<T>& main, const std::unordered_set<T>& to_remove) {
     std::unordered_set<T> result;
@@ -98,60 +119,61 @@ namespace {
     return result;
   }
   
-  const char* str(const char* data) {
-    if(data == nullptr){
-      return "NULL";
-    }
-    
-    return data;
+  // Get string representation in case data is nullptr. Makes it easier to print debugs
+  inline const char* str(const char* data) {
+    return data == nullptr ? "NULL" : data;
   }
 }
 
 unsigned long jnp1::poset_new(void) {
   DEBUG << __func__ << "()" << std::endl;
+  
   unsigned long newId = get_new_id(poset_counter());
   posets().emplace(newId, new poset_t());
+  
   DEBUG << __func__ << ": poset " << newId << " created" << std::endl;
   return newId;
 }
 
 void jnp1::poset_delete(unsigned long id) {
   DEBUG << __func__ << "(" << id << ")" << std::endl;
-  try {
-    poset_t *poset = posets().at(id);
-    poset_clear_elements(poset);
-    posets().erase(id);
-    free_id(poset_counter(), id);
-    DEBUG << __func__ << ": poset " << id << " deleted" << std::endl;
-  } catch (const std::out_of_range &e) {
+  
+  poset_t* poset = poset_try_get(id);
+  if(poset == nullptr) {
     DEBUG << __func__ << ": poset " << id << " does not exist" << std::endl;
     return;
   }
+  
+  poset_clear_elements(poset);
+  posets().erase(id);
+  free_id(poset_counter(), id);
+  
+  DEBUG << __func__ << ": poset " << id << " deleted" << std::endl;
 }
 
 size_t jnp1::poset_size(unsigned long id) {
   DEBUG << __func__ << "(" << id << ")" << std::endl;
-  try {
-    poset_t *poset = posets().at(id);
-    DEBUG << __func__ << ": poset " << id << " contains " << poset->size() << " element(s)" << std::endl;
-    return poset->size();
-  } catch (const std::out_of_range &e) {
+  
+  poset_t* poset = poset_try_get(id);
+  if(poset == nullptr){
     DEBUG << __func__ << ": poset " << id << " does not exist" << std::endl;
     return 0;
   }
+  
+  DEBUG << __func__ << ": poset " << id << " contains " << poset->size() << " element(s)" << std::endl;
+  return poset->size();
 }
 
 bool jnp1::poset_insert(unsigned long id, char const *value) {
   DEBUG << __func__ << "(" << id << ", \"" << str(value) <<  "\")" << std::endl;
+  
   if (value == nullptr) {
     DEBUG << __func__ << ": invalid value (NULL)" << std::endl;
     return false;
   }
 
-  poset_t *poset;
-  try {
-    poset = posets().at(id);
-  } catch (const std::out_of_range &e) {
+  poset_t *poset = poset_try_get(id);
+  if(poset == nullptr) {
     DEBUG << __func__ << ": poset " << id << " does not exist" << std::endl;
     return false;
   }
@@ -164,8 +186,8 @@ bool jnp1::poset_insert(unsigned long id, char const *value) {
 
   char *data = strdup(value);
   s = std::string_view(data);
-
   element_id_t newId = get_new_id(element_counter());
+  
   (*poset)[s] = newId;
   elements()[newId] = new poset_element_t();
 
@@ -175,15 +197,14 @@ bool jnp1::poset_insert(unsigned long id, char const *value) {
 
 bool jnp1::poset_remove(unsigned long id, char const *value) {
   DEBUG << __func__ << "(" << id << ", \"" << str(value) << "\")" << std::endl;
+  
   if (value == nullptr) {
     DEBUG << __func__ << ": invalid value (NULL)" << std::endl;
     return false;
   }
 
-  poset_t *poset;
-  try {
-    poset = posets().at(id);
-  } catch (const std::out_of_range &e) {
+  poset_t *poset = poset_try_get(id);
+  if(poset == nullptr){
     DEBUG << __func__ << ": poset " << id << " does not exist" << std::endl;
     return false;
   }
@@ -204,29 +225,26 @@ bool jnp1::poset_remove(unsigned long id, char const *value) {
     std::get<0>(*elements()[elem]).erase(id_to_remove);
   }
 
-  DEBUG << __func__ << ": poset " << id << ", element \"" << it->first.data() << "\" removed" << std::endl;
-
   delete_underlying_string(it->first);
-  free_id(element_counter(), id_to_remove);
-
   poset->erase(it);
   elements().erase(id_to_remove);
+  free_id(element_counter(), id_to_remove);
 
+  DEBUG << __func__ << ": poset " << id << ", element \"" << value << "\" removed" << std::endl;
   return true;
 }
 
 bool jnp1::poset_add(unsigned long id, char const *value1, char const *value2) {
-  DEBUG << __func__ << "(" << id << ", \"" << str(value1) << "\", \"" << str(value2) << "\")" << std::endl; 
+  DEBUG << __func__ << "(" << id << ", \"" << str(value1) << "\", \"" << str(value2) << "\")" << std::endl;
+
   if (value1 == nullptr || value2 == nullptr){
     if(value1 == nullptr) { DEBUG << __func__ << ": invalid value1 (NULL)" << std::endl; }
     if(value2 == nullptr) { DEBUG << __func__ << ": invalid value2 (NULL)" << std::endl; }
     return false;
   }
 
-  poset_t *poset;
-  try {
-    poset = posets().at(id);
-  } catch (const std::out_of_range &e) {
+  poset_t *poset = poset_try_get(id);
+  if(poset == nullptr) {
     DEBUG << __func__ << ": poset " << id << " does not exist" << std::endl;
     return false;
   }
@@ -264,26 +282,24 @@ bool jnp1::poset_add(unsigned long id, char const *value1, char const *value2) {
   }
 
   DEBUG << __func__ << ": poset " << id << ", relation (\"" << s1.data() << "\", \"" << s2.data() << "\") added" << std::endl;
-  
   return true;
 }
 
 bool jnp1::poset_del(unsigned long id, char const *value1, char const *value2) {
-  DEBUG << __func__ << "(" << id << ", \"" << str(value1) << "\", \"" << str(value2) << "\")" << std::endl; 
+  DEBUG << __func__ << "(" << id << ", \"" << str(value1) << "\", \"" << str(value2) << "\")" << std::endl;
+
   if (value1 == nullptr || value2 == nullptr){
     if(value1 == nullptr) { DEBUG << __func__ << ": invalid value1 (NULL)" << std::endl; }
     if(value2 == nullptr) { DEBUG << __func__ << ": invalid value2 (NULL)" << std::endl; }
     return false;
   }
 
-  poset_t *poset;
-  try {
-    poset = posets().at(id);
-  } catch (const std::out_of_range &e) {
+  poset_t *poset = poset_try_get(id);
+  if(poset == nullptr){
     DEBUG << __func__ << ": poset " << id << " does not exist" << std::endl;
     return false;
   }
-  
+
   std::string_view s1 = std::string_view(value1);
   std::string_view s2 = std::string_view(value2);
   auto elem1 = poset->find(s1);
@@ -310,23 +326,20 @@ bool jnp1::poset_del(unsigned long id, char const *value1, char const *value2) {
   std::get<0>(*elements()[elem2->second]).erase(elem1->second);
 
   DEBUG << __func__ << ": poset " << id << ", relation (\"" << s1.data() << "\", \"" << s2.data() << "\") deleted" << std::endl;
-  
   return true;
-
 }
 
 bool jnp1::poset_test(unsigned long id, char const *value1, char const *value2) {
   DEBUG << __func__ << "(" << id << ", \"" << str(value1) << "\", \"" << str(value2) << "\")" << std::endl; 
+
   if (value1 == nullptr || value2 == nullptr){
     if(value1 == nullptr) { DEBUG << __func__ << ": invalid value1 (NULL)" << std::endl; }
     if(value2 == nullptr) { DEBUG << __func__ << ": invalid value2 (NULL)" << std::endl; }
     return false;
   }
   
-  poset_t *poset;
-  try {
-    poset = posets().at(id);
-  } catch (const std::out_of_range &e) {
+  poset_t *poset = poset_try_get(id);
+  if(poset == nullptr){
     DEBUG << __func__ << ": poset " << id << " does not exist" << std::endl;
     return false;
   }
@@ -345,24 +358,23 @@ bool jnp1::poset_test(unsigned long id, char const *value1, char const *value2) 
     DEBUG << __func__ << ": poset " << id << ", relation (\"" << s1.data() << "\", \"" << s2.data() << "\") exists" << std::endl;
     return true;
   }
-
-  if (poset_test_relation(elem1->second, elem2->second)) {
-    DEBUG << __func__ << ": poset " << id << ", relation (\"" << s1.data() << "\", \"" << s2.data() << "\") exists" << std::endl;
-    return true;
-  } else {
-    DEBUG << __func__ << ": poset " << id << ", relation (\"" << s1.data() << "\", \"" << s2.data() << "\") does not exist" << std::endl;
-    return false;
-  }
+  
+  bool result = poset_test_relation(elem1->second, elem2->second);
+  
+  DEBUG << __func__ << ": poset " << id << ", relation (\"" << s1.data() << "\", \"" << s2.data() << "\") " << (result ? "exists" : "does not exist") << std::endl;
+  return result;
 }
 
 void jnp1::poset_clear(unsigned long id) {
   DEBUG << __func__ << "(" << id << ")" << std::endl;
-  try {
-    poset_t *poset = posets().at(id);
-    poset_clear_elements(poset);
-    DEBUG << __func__ << ": poset " << id << " cleared" << std::endl;
-  } catch (const std::out_of_range &e) {
+  
+  poset_t* poset = poset_try_get(id);
+  if(poset == nullptr){
     DEBUG << __func__ << ": poset " << id << " does not exist" << std::endl;
     return;
   }
+  
+  poset_clear_elements(poset);
+  
+  DEBUG << __func__ << ": poset " << id << " cleared" << std::endl;
 }
