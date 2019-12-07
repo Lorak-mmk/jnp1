6acd37a94b64ca09e096967b521ee428e4eea63c
Karol Baryła <lorak.mmk+kde@gmail.com>
2019-12-07 20:23:45 +0100

    Changed compute() method to value member to speed up computation

--- zadanie4/fibin.h
+++ zadanie4/fibin.h
@@ -1,10 +1,11 @@
 #ifndef FIBIN_H
 #define FIBIN_H
 
-#include <cstddef>
+#include <cstdint>
 
-static constexpr size_t Var(const char* name) {
-    size_t res = 0;
+//TODO: check if variable name is good
+static constexpr uint64_t Var(const char* name) {
+    uint64_t res = 0;
 
     while (*name) {
         char ch = *name;
@@ -22,29 +23,22 @@ static constexpr size_t Var(const char* name) {
 struct True {};
 struct False {};
 
-template <size_t N>
+template <uint64_t N>
 struct Fib {
-    // TODO iteracyjnie
     template <typename ValueType>
-    static constexpr ValueType compute() {
-        return Fib<N - 1>::template compute<ValueType>() + Fib<N - 2>::template compute<ValueType>();
-    }
+    static const ValueType value = Fib<N - 1>::template value<ValueType> + Fib<N - 2>::template value<ValueType>;
 };
 
 template <>
 struct Fib<0> {
     template <typename ValueType>
-    static constexpr ValueType compute() {
-        return ValueType(0);
-    }
+    static const ValueType value = ValueType(0);
 };
 
 template <>
 struct Fib<1> {
     template <typename ValueType>
-    static constexpr ValueType compute() {
-        return ValueType(1);
-    }
+    static const ValueType value = ValueType(1);
 };
 
 #endif
