7149d559f4caea5225b5cafeac7816448433c956
Karol Baryła <lorak.mmk+kde@gmail.com>
2020-01-20 18:59:53 +0100

    Implemented OddEvenMode

--- zadanie6/OddEvenMode.cc
+++ zadanie6/OddEvenMode.cc
@@ -1,7 +1,15 @@
 #include "OddEvenMode.h"
 
 std::vector<std::shared_ptr<IPlayable>> OddEvenMode::createOrder(std::vector<std::shared_ptr<IPlayable>> elements) {
-    return elements;
+    std::vector<std::shared_ptr<IPlayable>> result;
+    for(size_t i = 0; i < elements.size(); i += 2) {
+        result.push_back(elements[i]);
+    }
+    for(size_t i = 1; i < elements.size(); i += 2) {
+        result.push_back(elements[i]);
+    }
+
+    return result;
 }
 
 std::shared_ptr<IPlayMode> createOddEvenMode() {
