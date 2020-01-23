653fe4f4a9b09c8ed1100f4951df6d00acde0924
Karol Baryła <lorak.mmk+kde@gmail.com>
2020-01-23 21:08:33 +0100

    Fixed bug in OddEven mode

--- zadanie6/OddEvenMode.cc
+++ zadanie6/OddEvenMode.cc
@@ -3,11 +3,10 @@
 
 std::vector<std::shared_ptr<IPlayable>> OddEvenMode::createOrder(std::vector<std::shared_ptr<IPlayable>> elements) {
     std::vector<std::shared_ptr<IPlayable>> result;
-    for (size_t i = 0; i < elements.size(); i += 2) {
+    for (size_t i = 1; i < elements.size(); i += 2) {
         result.push_back(elements[i]);
     }
-
-    for (size_t i = 1; i < elements.size(); i += 2) {
+    for (size_t i = 0; i < elements.size(); i += 2) {
         result.push_back(elements[i]);
     }
 
